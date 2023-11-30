f7d7fb243a11fca8fbdd25e2474f8cdde2a1fcd9
Gan Mahmud <gan.mahmud@gmail.com>
2023-11-30 17:04:21 +0600

    🚀 Implementd index action to render resource data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < ActionController::API
   before_action :load_data
 
   def index
-    # ...
+    resource_key = params[:resource]
+    if @data.key?(resource_key)
+      render json: @data[resource_key]
+    else
+      render json: { error: "#{params[:resource]} not found" }, status: 404
+    end
   end
+
 
   def full_json
     # ...
