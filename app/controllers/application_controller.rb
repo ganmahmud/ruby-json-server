51593e54450019727e8b95b3d044720f6b01718d
Gan Mahmud <gan.mahmud@gmail.com>
2023-11-30 17:03:06 +0600

    🎬 Initialized ApplicationController with basic actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,48 @@
 class ApplicationController < ActionController::API
+  before_action :load_data
+
+  def index
+    # ...
+  end
+
+  def full_json
+    # ...
+  end
+
+  def show
+    # ...
+  end
+
+  def create
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def destroy
+    # ...
+  end
+
+  private
+
+  def load_data
+    # ...
+  end
+
+  private
+
+  def save_data
+    # ...
+  end
+
+  # Utils
+  def find_index_by_id(resource_data, id)
+    # ...
+  end
+
+  def find_item_by_field(resource_data, field, value)
+    # ...
+  end
 end
