86b6ba9582a27b6857ee84e5a7333726ec7218a9
Gan Mahmud <gan.mahmud@gmail.com>
2023-11-30 16:58:22 +0600

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
+# Did not use "Resourceful route" for better readability
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'application#full_json'
+  get '/:resource', to: 'application#index'
+  get '/:resource/:id', to: 'application#show'
+  post '/:resource', to: 'application#create'
+  patch '/:resource/:id', to: 'application#update'
+  delete '/:resource/:id', to: 'application#destroy'
 end
