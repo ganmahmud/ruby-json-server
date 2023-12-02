19dd5cf116a2608d693c4e7b9d6eb18dd3c6ea58
Gan Mahmud <gan.mahmud@gmail.com>
2023-12-02 15:29:11 +0600

    Added test JSON file

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Set a specific test data file for the controller
+  config.test_data_file_path = "#{Rails.root}/public/db-test.json"
 end
